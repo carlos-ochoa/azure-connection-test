e494fa161aa90eca191d11c75951af83952d7750
charlie-ochoa <carlos.ochoa@ab-inbev.com>
2022-02-08 10:34:12 -0600

    Changed BQ account service

--- main.py
+++ main.py
@@ -3,20 +3,11 @@ import pandas_gbq
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
-data = {
-    'PersonID' : [1,2,3],
-    'LastName' : ['Ochoa','Rico','Forero'],
-    'FirstName' : ['Carlos','Juan','Juliana'],
-    'Address' : ['1','2','3'],
-    'City' : ['CDMX','Bogota','Bogota']
-}
 
-query = """SELECT * FROM linen-cipher-312918.test.Persons"""
+query = """SELECT * FROM fh-bigquery.nyc.traffic_tickets LIMIT 10"""
 
-df = pd.DataFrame(data)
-
-table = "linen-cipher-312918.test.Persons"
-key_path = "linen-cipher-312918-7f6148510d0b.json"
+table = "azure-performance-test.testAZ.parking violations"
+key_path = "azure-performance-test-98fd0d574ffe.json"
 
 print("Authenticating with GCP...")
 
@@ -25,16 +16,19 @@ credentials = service_account.Credentials.from_service_account_file(
 )
 
 pandas_gbq.context.credentials = credentials
-pandas_gbq.context.project = "linen-cipher-312918"
+pandas_gbq.context.project = "azure-performance-test"
 
-print("Writing Data to Test Table with pandas-gbq...")
 
-df.to_gbq(table, if_exists = 'replace')
+
+#df.to_gbq(table, if_exists = 'replace')
 
 print("Reading Data from Test Table with pandas-gbq...")
 
 df2 = pd.read_gbq(query)
 print(df2)
+
+print("Writing Data to Test Table with pandas-gbq...")
+df2.to_gbq(table, if_exists = 'replace')
 
 print("Performing a reading query with BigQuery client...")
 client = bigquery.Client(credentials=credentials, project=credentials.project_id,)
